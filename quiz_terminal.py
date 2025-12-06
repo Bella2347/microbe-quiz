ac0a479e58a3604b236c5239a4b356cf5c0114ca
Bella <bsm.sinclair@gmail.com>
2025-12-06 17:37:22 +0100

    Moved functions to other script.

--- quiz_terminal.py
+++ quiz_terminal.py
@@ -4,8 +4,7 @@ import json
 import os
 import string
 import numpy as np
-
-ANSWER_OPTIONS = ["A", "B", "C", "D", "E"]
+from quiz_logic import load_microbes, load_questions, get_best_microbe_match
 
 
 def quiz():
@@ -19,24 +18,6 @@ def quiz():
     microbe, microbe_dict = get_best_microbe_match(microbes, answer)
 
     print_result(microbe, microbe_dict)
-
-
-def load_questions() -> dict:
-    """Load questions and answer options."""
-
-    with open("config/questions.json", "r", encoding="utf-8") as f:
-        questions = json.load(f)
-
-    return questions
-
-
-def load_microbes() -> dict:
-    """Load microbes."""
-
-    with open("config/microbes.json", "r", encoding="utf-8") as f:
-        microbes = json.load(f)
-
-    return microbes
 
 
 def run_quiz(personality_questions: dict) -> dict:
@@ -55,9 +36,7 @@ def run_quiz(personality_questions: dict) -> dict:
 
         for question, options in questions.items():
             question_nr += 1
-            answer_options = [
-                string.ascii_uppercase[i] for i in range(len(options))
-            ]
+            answer_options = [string.ascii_uppercase[i] for i in range(len(options))]
 
             clear()
             print(f"\n{question_nr}. {question}\n")
@@ -67,9 +46,7 @@ def run_quiz(personality_questions: dict) -> dict:
 
             while True:
                 answer = (
-                    input(
-                        f"\nEnter your choice ({', '.join(answer_options)}): "
-                    )
+                    input(f"\nEnter your choice ({', '.join(answer_options)}): ")
                     .strip()
                     .upper()
                 )
@@ -78,9 +55,7 @@ def run_quiz(personality_questions: dict) -> dict:
                     trait_score += answer_options.index(answer)
                     break
                 else:
-                    print(
-                        f"Invalid input. Please enter: {', '.join(answer_options)}."
-                    )
+                    print(f"Invalid input. Please enter: {', '.join(answer_options)}.")
 
         answers[personality_trait] = trait_score
 
@@ -94,35 +69,6 @@ def clear() -> None:
         os.system("cls")
     else:  # For Unix-based systems (Linux/macOS)
         os.system("clear")
-
-
-def get_best_microbe_match(
-    microbes: dict, answer: dict
-) -> tuple[str, dict]:
-    """Match answers to a microbe.
-
-    Answer is an array of six values, ranging from 0-6. The best matching
-    microbe is found by calculating the distance to each microbes answer
-    array and returning the closes one.
-    """
-    # Convert answers to a NumPy array
-    answer_array = np.array(list(answer.values()))
-
-    # Convert all personality answer sets to a 2D NumPy array
-    trait_arrays = np.array(
-        [list(p["answers"].values()) for p in microbes.values()]
-    )
-
-    # Compute Euclidean distances
-    distances = np.linalg.norm(trait_arrays - answer_array, axis=1)
-
-    # Find the index of the closest personality
-    closest_index = np.argmin(distances)
-
-    # Map back to the personality key
-    microbe = list(microbes.keys())[closest_index]
-
-    return microbe, microbes[microbe]
 
 
 def print_result(microbe: str, microbe_dict: dict) -> None:
